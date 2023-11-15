f2b6af2b3028c8b582389c17f6243366451c575a
MASFUAD12 <147736994+MASFUAD12@users.noreply.github.com>
2023-11-15 15:01:42 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -79,14 +79,6 @@ echo "$localip $(hostname)" >> /etc/hosts
 fi
 mkdir -p /etc/xray
 
-echo -e "${tyblue} Welcome To Cobek Sawit Tunnel${NC} "
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Preparing the install file"
-apt install git curl -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] installation file is ready"
-sleep 2
-
-
 # // cek old script
 if [[ -r /etc/xray/domain ]]; then
 clear
